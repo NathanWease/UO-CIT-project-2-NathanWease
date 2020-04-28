20f327a6fb5606f6b729f6af5fa5534414a2c155
jchacko15 <63620708+jchacko15@users.noreply.github.com>
2020-04-27 23:55:03 -0700

    Create articles-api.js

---
+++ articles-api.js
@@ -0,0 +1,31 @@
+//jshint esversion: 6
+
+let requestURL = "https://thejsway-server.herokuapp.com/api/articles";
+
+//clear old photos and files
+
+$.getJSON(requestURL, function(response) {
+  console.log(response);
+
+  console.log(`Article ${response[0].id}
+    Title: ${response[0].title}
+    Content: ${response[0].comment}`);
+
+    response.forEach(function(article, index) {
+      let newDiv = document.createElement("div");
+      document.querySelector(".articles").appendChild(newDiv);
+
+      let header = document.createElement("h3");
+      header.textContent = `Article ${article.id}`;
+      newDiv.appendChild(header);
+
+      let titlePar = document.createElement("p");
+      titlePar.textContent = article.title;
+      newDiv.appendChild(titlePar);
+
+      let contentPar = document.createElement("p");
+      contentPar.textContent = article.content;
+      newDiv.appendChild(contentPar);
+
+    };
+};
